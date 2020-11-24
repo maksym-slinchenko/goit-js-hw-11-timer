442dc6bc104e120b0d6ba68a0fddb8f5b82e3041
Maksym Slinchenko <max.slin777@gmail.com>
2020-11-24 22:35:52 +0200

    Разметка + стили, но без класса

--- src/index.js
+++ src/index.js
@@ -2,55 +2,55 @@ import "./css/styles.css";
 import getTemplate from "./temlates/page-layout.hbs";
 
 const timer = {
-    selector: "#timer-1",
-    targetDate: new Date("Jul 17, 2019")
-  },
+  selector: "#timer-1",
+  targetDate: new Date("Jan 01, 2021"),
+};
 
-//  переменные;
+// Переменные;
 const timerEl = document.querySelector(timer.selector);
 const startdeltaTime = timer.targetDate - Date.now();
+console.log(timerEl);
+// Функция добавления "0" в значение счетчика
+const pad = function (value, symbolNumbers) {
+  return String(value).padStart(symbolNumbers, "0");
+};
+
+// Функция создания массива значений счетчика
+const getDateArrey = function (deltaTime) {
+  let dates = [];
+  return (dates = [
+    {
+      label: "Дней",
+      dataValue: "days",
+      value: pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)), 3),
+    },
+    {
+      label: "Часов",
+      dataValue: "hours",
+      value: pad(
+        Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        2
+      ),
+    },
+    {
+      label: "Минут",
+      dataValue: "mins",
+      value: pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)), 2),
+    },
+    {
+      label: "Секунд",
+      dataValue: "secs",
+      value: pad(Math.floor((deltaTime % (1000 * 60)) / 1000), 2),
+    },
+  ]);
+};
+// Массив объектов текущей даты
 const startDates = getDateArrey(startdeltaTime);
- // Функция добавления "0" в значение счетчика
- const pad = function (value, symbolNumbers) {
-    return String(value).padStart(symbolNumbers, "0");
-  };
-
-
-  // Функция создания массива значений счетчика
-  const getDateArrey = function (deltaTime) {
-    let dates = [];
-    return (dates = [
-      {
-        label: "Days",
-        dataValue: "days",
-        value: pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)), 3),
-      },
-      {
-        label: "Hours",
-        dataValue: "hours",
-        value: pad(
-          Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-          2
-        ),
-      },
-      {
-        label: "Minutes",
-        dataValue: "mins",
-        value: pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)), 2),
-      },
-      {
-        label: "Seconds",
-        dataValue: "secs",
-        value: pad(Math.floor((deltaTime % (1000 * 60)) / 1000), 2),
-      },
-    ]);
-  };
-  //  Функция наполнения структуры документа
- 
-    timerEl.insertAdjacentHTML("afterbegin", timer.template(startDates));
-  
-  // Функция внесения значений таймера
-insertTimerValue = function (dates) {
+console.log(startDates);
+//  Функция наполнения структуры документа
+timerEl.insertAdjacentHTML("beforeend", getTemplate(startDates));
+// Функция внесения значений таймера
+const insertTimerValue = function (dates) {
   for (let i = 0; i < dates.length; i += 1) {
     if (
       dates[i].dataValue ===
@@ -66,11 +66,11 @@ insertTimerValue = function (dates) {
 };
 insertTimerValue(startDates);
 
+setInterval(() => {
   const currentdeltaTime = timer.targetDate - Date.now();
   const currentDates = getDateArrey(currentdeltaTime);
   insertTimerValue(currentDates);
-
-  
+}, 1000);
 
 // class CountdownTimer {
 //   constructor({ selector, targetDate }, template) {
@@ -152,4 +152,3 @@ insertTimerValue(startDates);
 //   selector: "#timer-1",
 //   targetDate: new Date("Jul 17, 2019"),
 // }, template: getTemplate);
-
